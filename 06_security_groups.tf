6009bb56b102b557c83863db666ae4a3f5ec361a
pranavp12 <phalnikar.pranav@gmail.com>
2019-11-17 20:34:23 +0000

    Update 06_security_groups.tf

--- 06_security_groups.tf
+++ 06_security_groups.tf
@@ -92,3 +92,12 @@ resource "aws_security_group_rule" "pvt_subnet_outbound_all" {
   cidr_blocks       = ["0.0.0.0/0"]
   description       = "allow all outbound traffic from private subnet"
 }
+resource "aws_security_group_rule" "pub_subnet_outbound_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = "${aws_security_group.sg_pub.id}"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "allow all outbound traffic from private subnet"
+}
