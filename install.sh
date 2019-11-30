51c2bae8c5cd33cee248edc4509c34308548de11
pranavp12 <phalnikar.pranav@gmail.com>
2019-11-30 15:10:57 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo yum install java-1.8.0-openjdk -y
+sudo yum install java-1.8.0-openjdk-devel.x86_64 -y
 #sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 #sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 #sudo yum install -y jenkins
