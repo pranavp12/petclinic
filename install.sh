028af0271c4936d37a334c03dc9d375f68260fd5
pranavp12 <phalnikar.pranav@gmail.com>
2019-11-30 11:38:23 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo yum install java-1.8.0-openjdk -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum install -y jenkins
+#sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+#sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+#sudo yum install -y jenkins
+wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
+nohup java -jar jenkins.war --httpPort=9090 &
 
 sudo yum install python3 -y
 sudo yum -y install python3-pip
