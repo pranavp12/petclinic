94dcfe47c6e4857380ba99a111bfd4f8af12e657
pranavp12 <phalnikar.pranav@gmail.com>
2019-12-02 02:47:26 +0000

    Update 07_pvt_ins.tf

--- 07_pvt_ins.tf
+++ 07_pvt_ins.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "aws_ins_pvt" {
     ami = "${lookup(var.amis, var.aws_region)}"
     instance_type = "t2.micro"
-    key_name = "deployer-key"
+    key_name = "ee_key"
     vpc_security_group_ids = ["${aws_security_group.sg_pvt.id}"]
     subnet_id = "${aws_subnet.private_subnet.id}"
     associate_public_ip_address = false
